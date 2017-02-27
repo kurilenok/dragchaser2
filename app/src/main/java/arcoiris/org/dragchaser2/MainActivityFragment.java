936112baba7e5e7ce9d178b278e8f1bef7c75fe8
kurilenok <kurilenok@gmail.com>
2017-02-27 23:10:45 +0300

    first

---
+++ app/src/main/java/arcoiris/org/dragchaser2/MainActivityFragment.java
@@ -0,0 +1,54 @@
+package arcoiris.org.dragchaser2;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    @BindView(R.id.tvFirebase)
+    TextView tvFirebase;
+
+
+    public MainActivityFragment() {
+
+        FirebaseDatabase fdb = FirebaseDatabase.getInstance();
+        DatabaseReference ref = fdb.getReference("node");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tvFirebase.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}
